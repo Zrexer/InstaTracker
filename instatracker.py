01f810aee9d9fc6ae111e99a0b928663b2953806
Zrexer <122536817+Zrexer@users.noreply.github.com>
2023-11-19 14:57:43 +0330

    Update instatracker.py

--- instatracker.py
+++ instatracker.py
@@ -30,6 +30,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
+__version__ = '0.0.2'
+
 import requests
 import pystyle
 
@@ -83,83 +85,21 @@ class Tracker:
             reqINFO = dict(requests.get(f'https://api2.haji-api.ir/instainfo/?text={username}').json()).get('result')[0]
         except Exception as EREQ:
             return EREQ
-
-        pystyle.Write.Print("{:<20} {:<}".format("Selected ID", username), pystyle.Colors.yellow_to_red, 0)
-        print()
-
-        try:
-            fullName = str(reqINFO.get('full_name'))
-            pystyle.Write.Print("{:<20} {:<}".format("full name" ,fullName), pystyle.Colors.yellow_to_red, 0)
-            print()
-        except:
-            pass
-
-        try:
-            biog = str(reqINFO.get('biography'))
-            pystyle.Write.Print("{:<20} {:<}".format("biography",biog), pystyle.Colors.yellow_to_red, 0)
-            print()
-        except:
-            pass
-
-        try:
-            can_add_fb_group_link_on_profile = str(reqINFO.get('can_add_fb_group_link_on_profile'))
-            pystyle.Write.Print("{:<20} {:<}".format("can add fb group link on profile", can_add_fb_group_link_on_profile), pystyle.Colors.yellow_to_red, 0)
-            print()
-        except:
-            pass
-
-        try:
-            fbid_v2 = str(reqINFO.get('fbid_v2'))
-            pystyle.Write.Print("{:<20} {:<}".format("fbid v2",fbid_v2), pystyle.Colors.yellow_to_red, 0)
-            print()
-        except:
-            pass
-
-        try:
-            pk = str(reqINFO.get('pk'))
-            pystyle.Write.Print("{:<20} {:<}".format("pk",pk), pystyle.Colors.yellow_to_red, 0)
-            print()
-        except:
-            pass
-
-        try:
-            biolinks = str(reqINFO.get('bio_links'))
-            pystyle.Write.Print("{:<20} {:<}".format("bio links json",biolinks), pystyle.Colors.yellow_to_red, 0)
-            print()
-        except:
-            pass
             
-        try:
-            followerC = str(reqINFO.get('follower_count'))
-            pystyle.Write.Print("{:<20} {:<}".format("follower count" ,followerC), pystyle.Colors.yellow_to_red, 0)
+        for k, v in reqINFO.items():
+            pystyle.Write.Print('{:<30} {:<}'.format(str(k), str(v)), pystyle.Colors.yellow_to_red, 0)
             print()
-        except:
-            pass
-            
-        try:
-            followingC = str(reqINFO.get('following_count'))
-            pystyle.Write.Print("{:<20} {:<}".format("following count", followingC), pystyle.Colors.yellow_to_red, 0)
-            print()
-        except:
-            pass
-            
-        try:
-            has_anon_profile = str(reqINFO.get('has_anonymous_profile_picture'))
-            pystyle.Write.Print("{:<20} {:<}".format("Anonymous profile",has_anon_profile), pystyle.Colors.yellow_to_red, 0)
-            print()
-        except:
-            pass
 
 class Help:
     
     def usage():
-        pystyle.Write.Print('{:<20} {:<}'.format("Command", 'Usage'), pystyle.Colors.green_to_yellow, 0.01)
+        pystyle.Write.Print('{:<30} {:<}'.format("Command", 'Usage'), pystyle.Colors.green_to_yellow, 0.01)
         print()
         pystyle.Write.Print("-"*30, pystyle.Colors.green_to_yellow, 0)
         print()
-        pystyle.Write.Print('{:<20} {:<}'.format('help', 'show this message'), pystyle.Colors.green_to_yellow, 0)
+        pystyle.Write.Print('{:<30} {:<}'.format('help', 'show this message'), pystyle.Colors.green_to_yellow, 0)
         print()
-        pystyle.Write.Print('{:<20} {:<}'.format('track', 'Start Tracking: track ali || track @ali [you can use @ or not]'), pystyle.Colors.green_to_yellow, 0)
+        pystyle.Write.Print('{:<30} {:<}'.format('track', 'Start Tracking: track ali || track @ali [you can use @ or not]'), pystyle.Colors.green_to_yellow, 0)
         print()
         pystyle.Write.Print("\nMore About Track: if you want to save results, just use this: [ track ali / @ali -s / --save :[you can set name for that after -s or --save command:[optional]]]", pystyle.Colors.purple_to_red, 0)
         print()
@@ -184,65 +124,42 @@ class MainACT:
                 else:
                     if "-s" in text:
                         if "-s" == text[-1]:
-                            try:
-                                with open('resultsTracker.txt', 'a') as ss:
-                                    user = text[text.index('track')+1]
-                                    res = Tracker.start(user)
-                                    ss.write(res)
-                                    ss.close()
-                                 
-                                pystyle.Write.Print('File Saved in resultsTracker.txt', pystyle.Colors.purple_to_red, 0.01)
-                                print()
-                            except Exception as E:
-                                print(E)
-                                pass
-                    
-                        else:
-                            try:
-                                sn = open(text[text.index('-s')+1], 'a')
+                            with open('resultsTracker.txt', 'a') as ss:
                                 user = text[text.index('track')+1]
                                 res = Tracker.start(user)
-                                sn.write(res)
-                                sn.close()
-                                pystyle.Write.Print(f'File Saved in {text[text.index("-s")+1]}', pystyle.Colors.purple_to_red, 0.01)
-                                print()
-                            
-                            except Exception as E:
-                                print(E)
-                                pass
+                                ss.write(res)
+                                ss.close()
+                                
+                            pystyle.Write.Print('File Saved in resultsTracker.txt', pystyle.Colors.purple_to_red, 0.01)
+                            print()
+                    
+                        else:
+                            sn = open(text[text.index('-s')+1], 'a')
+                            user = text[text.index('track')+1]
+                            res = Tracker.start(user)
+                            sn.write(res)
+                            sn.close()
+                            pystyle.Write.Print(f'File Saved in {text[text.index("-s")+1]}', pystyle.Colors.purple_to_red, 0.01)
+                            print()
             
                     elif "--save" in text:
-                        
                         if "--save" == text[-1]:
-                            try:
-                                with open('resultsTracker.txt', 'a') as ss:
-                                    user = text[text.index('track')+1]
-                                    res = Tracker.start(user)
-                                    ss.write(res)
-                                    ss.close()
-                                pystyle.Write.Print('File Saved in resultsTracker.txt', pystyle.Colors.purple_to_red, 0.01)
-                                print()
-
-                            except Exception as E:
-                                print(E)
-                                pass
-
-                    
-                        else:
-                            try:
-                                sn = open(text[text.index('--save')+1], 'a')
+                            with open('resultsTracker.txt', 'a') as ss:
                                 user = text[text.index('track')+1]
                                 res = Tracker.start(user)
-                                sn.write(res)
-                                sn.close()
-                                pystyle.Write.Print(f'File Saved in {text[text.index("--save")+1]}', pystyle.Colors.purple_to_red, 0.01)
-                                print()
-                            except Exception as E:
-                                print(E)
-                                pass
-            if "exit" in text:
-                exit()
-
-try:
-    MainACT.Main()
-except:pass
+                                ss.write(res)
+                                ss.close()
+                                
+                            pystyle.Write.Print('File Saved in resultsTracker.txt', pystyle.Colors.purple_to_red, 0.01)
+                            print()
+                    
+                        else:
+                            sn = open(text[text.index('--save')+1], 'a')
+                            user = text[text.index('track')+1]
+                            res = Tracker.start(user)
+                            sn.write(res)
+                            sn.close()
+                            pystyle.Write.Print(f'File Saved in {text[text.index("--save")+1]}', pystyle.Colors.purple_to_red, 0.01)
+                            print()
+        
+MainACT.Main()
